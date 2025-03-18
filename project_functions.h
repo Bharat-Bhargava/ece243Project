cbf8a906083d93a6c01949752e7b9239efb7ad70
Bharat Bhargava <bbhargava01@gmail.com>
2025-03-18 14:38:53 -0400

    removed useless line

--- project_functions.h
+++ project_functions.h
@@ -12,10 +12,4 @@ void initialize_timer(struct timer_t *timer, uint32 period);
 uint32 read_switches(void);
 uint32 read_buttons(void);
 
-void clear_screen(struct fb_t *const fbp, unsigned short color);
-
-void move_bat(struct fb_t *const fbp, unsigned short *bat[], int *bat_x,
-              int *bat_y, int *frame, uint32 key_state);
-void sprite_draw(struct fb_t *const fbp, unsigned short sprite[], int x, int y);
-
 #endif
